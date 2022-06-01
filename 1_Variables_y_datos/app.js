8234b2cc0bb5b132a2675a8f76c909aeee757361
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-05-31 21:02:07 -0500

    variables y tipos de datos

---
+++ 1_Variables_y_datos/app.js
@@ -0,0 +1,65 @@
+
+// Creación y asignación de valor a una variable
+var nombre;
+nombre = 'Hiromi';
+
+
+var nombreUsuario = 'Carlos';
+
+// STRINGS
+var pais = 'Colombia';
+var fraseSecreta = "Primer amigo de la infancia";
+var letra = 'a';
+var nombrePrincipal = 'H';
+
+var info = nombre + pais;
+console.log(info);
+// Las variables se pueden reasignar sin que las declaremos de nuevo
+pais = 'México';
+letra = 'x';
+
+// Podemos acceder a la información que está almacenada en las variables
+console.log('hola');
+console.log(fraseSecreta);
+console.log(nombreUsuario);
+console.log(nombrePrincipal);
+
+// number
+var edad
+
+edad = 20;
+
+var telefono = 11234;
+var costo = 20;
+var resultado = -50;
+var promedio = 9.87;
+
+// Las variables se pueden reasignar sin que las declaremos de nuevo
+costo = 10;
+resultado = 0;
+
+var numCelular = '+52 745w748';
+var numOficina = '23456';
+
+// Concatenar -> Unir dos o más Strings o dos o más valores
+
+var infoNueva = nombre + pais;
+console.log(infoNueva)
+console.log(nombreUsuario + ' ' + letra + ', hola');
+console.log(promedio + nombrePrincipal)
+console.log('Mi nombre es ' + nombrePrincipal + ' y mi promedio es ' + promedio)
+
+// PENDIENTE -> Concatenar con Template string
+
+// BOOLEAN
+var esAdulto = true;
+var completado = false;
+var graduado = false;
+var cumple = true;
+var Mexicano = false;
+
+// La reasignación de valores no diferencia los tipos de dato
+esAdulto = 100;
+nombre = true;
+
+
