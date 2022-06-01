8e4228b918bbba07ad046ccd53f3782ddb2f758e
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-05-31 20:23:05 -0500

    intro a Js

---
+++ 0_IntroJS/main.js
@@ -0,0 +1,31 @@
+// JavaScript es un lenguaje que se lee de arrriba a abajo
+
+// SALIDAS
+
+// Cómo poner una alerta desde Js
+// alert('Hola, mundo!')
+// alert('otro alert')
+
+// Enviar mensajes a la consola
+console.log('este mensaje se verá en la consola')
+console.log('otro mensaje')
+
+// Mostrar un error en la consola
+console.error('ERROR: No se encontró el archivo')
+
+// Mostrar un warning en la consola
+console.warn('Estás añadiendo muchas alertas')
+
+document.write('Texto desde Js')
+
+// ENTRADA
+// prompt('Introduce tu nombre')
+/*prompt('Introduce tu apellido')
+prompt('Introduce tu nombre')
+prompt('Introduce tu apellido')
+prompt('Introduce tu nombre')*/
+// prompt('Introduce tu apellido')
+// prompt('Introduce tu nombre')
+// prompt('Introduce tu apellido')
+
+
