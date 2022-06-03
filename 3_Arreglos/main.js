25d6eb10ec1c4346a9f165f2fedf8b4a0050c622
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-06-02 21:22:44 -0500

    arreglos

---
+++ 3_Arreglos/main.js
@@ -0,0 +1,60 @@
+// var nombres = ['Diana', 'Luisa', 'Diego', 'Julio'];
+
+// console.log(nombres);
+// console.log(nombres[0]);
+// console.log(nombres[1]);
+// console.log(nombres[2]);
+// console.log(nombres[3]);
+// console.log(nombres[4]);
+// nombres[2] = 'Carlos';
+// console.log(nombres);
+
+// nombres[4] = 'Hugo';
+// console.log(nombres);
+
+// nombres[10] = 'Ana';
+// console.log(nombres);
+
+// console.log('Me llamo ' + nombres[3]);
+
+// var calificaciones = [90, 100, 50, 100, 70, 85, 60, 40];
+
+// console.log(calificaciones);
+// console.log(calificaciones.length);
+// console.log(calificaciones[0]);
+// console.log(calificaciones[7]);
+// console.log(calificaciones[1] + calificaciones[4]);
+
+// var califMat = calificaciones[5];
+// console.log(califMat);
+
+// var info = [];
+// console.log(info)
+
+// LEER -> Clases y sus métodos
+
+
+var materias = ['matemáticas', 'física'];
+
+// Length es una PROPIEDAD de los arreglos que nos indica la cantidad de elementos que tiene
+
+console.log(materias)
+console.log(materias.length)
+
+// MÉDOTOS DE ARREGLOS
+
+// Push añade 1 elemento al final del arreglo
+materias.push('química');
+
+console.log(materias);
+
+materias.push('inglés');
+
+console.log(materias);
+
+// Pop elimina el último elemento del arreglo
+
+materias.pop();
+
+console.log(materias);
+
