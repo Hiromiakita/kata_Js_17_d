7caaebfb98c7b8d11ee4f2ce3bba989e2dd938ec
cchavezmx <cchavezmx@outlook.com>
2022-06-07 23:53:56 -0500

    ejercicios resueltos

---
+++ ejerciosResueltos/main.js
@@ -0,0 +1,50 @@
+// let exchange = prompt('Ingresa MXN y te devolvemos dolares')
+let mxn = 300;
+
+function change(a) {
+  let dolarPrice = 19.59;
+  let dollars = a / dolarPrice;
+	// alert(dollars)
+  return dollars;
+}
+
+// console.log(change(mxn))
+
+// Paco hizo travesuras en el salón y la maestra lo castigó, para poder salir tiene escribir en el pizarrón todos los números pares  del 1 al 100, ¿puedes ayudarle a Paco a hacer un algoritmo que haga esto por el?
+
+// function imprimirNumeros(){
+//   for (let i = 2; i <= 100; i+=2)
+//     console.log(i)
+// }
+
+// imprimirNumeros()
+// counter
+// let numeroPar = 1;
+
+// for(i = 0; i <= 100; i++){
+//   // usar siempre el triple igual
+// if (numeroPar % 2 === 0){
+//     console.log(numeroPar)
+// }
+//   numeroPar++
+// }
+
+
+// Escribir todos los números que terminan en 0 y estén entre 2 y 121
+// function imprimirNumeros(){
+//   for (let i = 10; i <= 121; i+=10)
+//     console.log(i)
+// }
+
+// imprimirNumeros()
+
+
+// let numeroPar = 10;
+// for(i = 0; i < 121; i++){
+//   // usar siempre el triple igual
+// if (numeroPar % 10 === 0){
+//     console.log(numeroPar)
+// }
+//   numeroPar++
+// }
+
