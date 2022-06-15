2c5d1bdf090b4f216ddc86f094e795d27f7e2cee
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-06-14 19:35:22 -0500

    challenge

---
+++ Challenge_JS/app.js
@@ -0,0 +1,45 @@
+let info = [
+    {
+        nombre: "Diego",
+        contrasenia: 123,
+        materias: [
+            {
+                nombreMateria: "español",
+                calificacion: 100
+            },
+            {
+                nombreMateria: "matemáticas",
+                calificacion: 70
+            },
+            {
+                nombreMateria: "ciencias",
+                calificacion: 55
+            }
+        ]
+    },
+    {
+        nombre: "Jorge",
+        contrasenia: 12345,
+        materias: [
+            {
+                nombreMateria: "español",
+                calificacion: 60
+            },
+            {
+                nombreMateria: "matemáticas",
+                calificacion: 65
+            },
+            {
+                nombreMateria: "ciencias",
+                calificacion: 80
+            }
+        ]
+    }
+]
+
+// 1. Un grupo de alumnos tiene todas sus calificaciones del semestre y quieren saber su información final, para lo cual, deben ingresar al portal de la escuela con su usuario y contraseña.
+// 2. Verán un menú de opciones
+    // 1. Ver todas su calificaciones.
+    // 2. Conocer su promedio.
+    // 3. Conocer su calificación más alta.
+    // 4. Conocer su calificación más baja.
