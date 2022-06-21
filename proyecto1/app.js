250c0f4baf61358defba0eae3fb3e316512d924c
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-06-21 18:53:39 -0500

    proyecto cajero

--- proyecto1/app.js
+++ proyecto1/app.js
@@ -10,61 +10,6 @@ let cuentas = [
   { nombre: "Carlos", saldo: 67, password: '123' }
 ];
 
-// login();
-
-// Inicio del programa
-// function login() {
-//     // 1. Elegir usuario.
-//     let usuario = prompt('Escribe tu nombre');
-//     let contra = prompt('Ingresa la contraseña para el usuario: ' + usuario);
-//     validarUsuario(usuario, contra)
-// }
-
-// function validarUsuario (usuario, contra) {
-//     for(let i = 0; i < cuentas.length; i++) {
-//         if(usuario === cuentas[i].nombre) {
-//             // Usuario existe
-//             if(contra === cuentas[i].password) {
-//                 console.log('DATOS CORRECTOS');
-//                 mostrarMenu(i)
-//             } else {
-//                 console.log('Contraseña incorrecta')
-//             } 
-//             return
-//         } else {
-//             console.log('no');
-//         }
-//     }
-// }
-
-// Al seleccionar una cuenta, debes ingresar el password asociado a la cuenta. Si el password es incorrecto, debes notificar al usuario y permitirle intentarlo nuevamente. Si el password es correcto, debes mostrar las siguientes opciones:
-// Consultar saldo
-// Ingresar monto
-// Retirar monto
-
-// function mostrarMenu(posicionUsuario){
-//     let opcion = prompt('Indica una opción:\n 1. Consultar saldo\n 2. Hacer un deposito \n 3. Hacer un retiro \n 4. Salir');
-
-//     if(opcion == 1) {
-//         consultarSaldo(posicionUsuario)
-//     } else if(opcion == 2) {
-//         depositar(posicionUsuario)
-//     } else if(opcion == 3){
-//         retirar(posicionUsuario)
-//     } else if(opcion == 4){
-//         alert('adiós');
-//         return
-//     } else {
-//         alert('opción inválida');
-//         mostrarMenu(posicionUsuario);
-//     }
-// }
-
- 
-// Al seleccionar consultar saldo, debe mostrar en pantalla el saldo actual de la cuenta
-// Al seleccionar ingresar monto, el usuario debe escribir el monto a ingresar. Al ingresar el monto, debe mostrarle al usuario el monto ingresado y el nuevo saldo total.
-// Al seleccionar retirar monto, el usuario debe escribir el monto a retirar. Al retirar el monto, debe mostrarle al usuario el monto retirado y el nuevo saldo total.
- 
 // function consultarSaldo(posicionUsuario) {
 //     alert('El saldo del usuario ' + cuentas[posicionUsuario].nombre + ' es de $' + cuentas[posicionUsuario].saldo);
 //     mostrarMenu(posicionUsuario)
@@ -86,14 +31,6 @@ let cuentas = [
 
 // Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10.
 // Es necesario hacer las validaciones pertinentes en tu código para que no se rompa esta regla de negocio.
-
-// RETO EXTRA PLUS:
-// Si ya posees conocimientos de desarrollo web con html, css y javascript. Haciendo uso del DOM crea una interfaz con html y css que maneje toda la interacción descrita arriba
-
-
-
-
-
 
 
 // ---------------------------------------------------------------------------
@@ -136,4 +73,36 @@ function mostrarMenuHTML(posicionUsuario){
     // Mostrar menú opciones
     document.getElementById("acciones").style.display = "block";
 
+    // Mostrar nombre en el saludo
+    // 1. obtener elemento a modificar a través de su id e insertar el nombre del usuario actual
+    document.getElementById("nombre-usuario").innerText = cuentas[posicionUsuario].nombre
+
+    // Añadir funciones a los botenes desde Js, con ayuda de addEventListener
+    // Consultar saldo
+    document.getElementById("consultar").addEventListener('click', function () {
+        // alert('El saldo actual es de: $' + cuentas[posicionUsuario].saldo);
+        // No usar un alert poner el saldo a consultar en un elemento HTML
+        // Opc1. Crear un elemento HTML
+
+        // Opc2. El elemento ya está creado en el HTML y sólo lo rellenamos
+        document.getElementById("info").innerText = 'El saldo actual es de: $' + cuentas[posicionUsuario].saldo;
+    } );
+
+    // Contruir el resto de funciones para los botones
+
+}
+
+// En lugar de usar esta función se construyó con un addEventListener 
+// function consultarSaldoHTML(posicionUsuario) {
+//     alert('El saldo actual es de: $' + cuentas[posicionUsuario].saldo);
+// }
+
+function retirarSaldoHTML(posicionUsuario) {
+    // Limpiar el texto info
+    document.getElementById("info").innerText = '';
+    alert('retirar de saldo');
+}
+
+function ingresarSaldoHTML(posicionUsuario) {
+    alert('ingresar de saldo');
 }
